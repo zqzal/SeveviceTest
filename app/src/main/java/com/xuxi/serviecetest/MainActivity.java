101a7abad79ba43e1a6a522667e15a5f8d8fc3c2
zqzal <936952020@qq.com>
2019-08-19 16:31:40 +0800

    star service and stop service

--- app/src/main/java/com/xuxi/serviecetest/MainActivity.java
+++ app/src/main/java/com/xuxi/serviecetest/MainActivity.java
@@ -1,13 +1,40 @@
 package com.xuxi.serviecetest;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startService = findViewById(R.id.start_service);
+        Button stopService = findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.start_service:
+                Intent startIntent = new Intent(this,MyService.class);
+                startService(startIntent); //启动服务
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this,MyService.class);
+                stopService(stopIntent); //停止服务
+                break;
+                default:
+                    break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/xuxi/serviecetest/MyService.java
@@ -0,0 +1,37 @@
+package com.xuxi.serviecetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private static final String TAG = "MyService";
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy executed");
+    }
+}
